8d5ee0d357979da856d88b4becc68141c07f0753
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-02-24 19:47:16 -0800

    reorganizing files and folders

--- algorithms/twoCharacters.js
+++ algorithms/twoCharacters.js
@@ -38,28 +38,6 @@
 // Thus, we print the length of babab, which is , as our answer.
 
 
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
-
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
-
-process.stdin.on('data', function (data) {
-    input_stdin += data;
-});
-
-process.stdin.on('end', function () {
-    input_stdin_array = input_stdin.split("\n");
-    main();    
-});
-
-function readLine() {
-    return input_stdin_array[input_currentline++];
-}
-
-/////////////// ignore above this line ////////////////////
-
 function removePairs(str) {
   var arr = str.split('')
   var rem = []
