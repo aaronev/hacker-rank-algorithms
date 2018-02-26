a0bd206f38559a5d454a9fd1ddb3bd4c725d0f17
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-02-25 20:50:00 -0800

    second attempt

--- algorithms/twoCharacters.js
+++ algorithms/twoCharacters.js
@@ -1,6 +1,27 @@
 // https://www.hackerrank.com/challenges/two-characters/problem
 
-function removePairs(str) {
+function removeOnes(str) {
+  var letters = {}
+  var posLtrs = []
+  var res = []
+  
+  for (let i = 0; i < str.length; i++) {
+    if (letters[str[i]]) letters[str[i]]++
+    else letters[str[i]] = 1
+  }
+  
+  for (let ltr in letters) {
+    if (letters[ltr] !== 1) posLtrs.push(ltr)
+  }
+  
+  for (let i = 0; i < str.length; i++) {
+    if (posLtrs.includes(str[i])) res.push(str[i])
+  }
+  
+  return res.join('')
+}
+
+function recurxRemovePairsAndOnes(str) {
   var arr = str.split('')
   var rem = []
   
@@ -11,48 +32,58 @@ function removePairs(str) {
   for (let i = 0; i < arr.length; i++) {
     if (rem.includes(arr[i])) arr[i] = ''
   }
-
-  var newStr = arr.join('')
+  
+  var newStr = removeOnes(arr.join(''))
   
   for (let i = 0; i < newStr.length - 1; i++) {
-    if (newStr[i] === newStr[i+1]) return removePairs(newStr)
+    if (newStr[i] === newStr[i+1]) return recurxRemovePairsAndOnes(newStr)
+    
   }
   
   return newStr
+  
 }
 
-function posSeqAndRemoveSingles(str) {
-  var letters = {}
-  var posSeq = []
-  var res = []
+function lettersDataStructure(s) {
+  var seq = {}
   
-  for (let i = 0; i < str.length; i++) {
-    if (letters[str[i]]) letters[str[i]]++
-    else letters[str[i]] = 1
+  for (let i = 0; i < s.length; i++) {
+    seq[s[i]] = {}
+  }
+    
+  //find a way to go through a string and add it
+  let counter = 0
+  
+  while (counter < Object.keys(seq).length) {
+    
+    for (let i = 0; i < s.length; i++) {
+      
+      if (s[counter] === s[i]) {
+        // if s[counter] equals s[i] then that's when you count the numbers that come after it
+        // you start at the particular position s[i]
+        for (let j = i+1; j < s.length; j++) {
+          if (s[counter] !== s[j]) {
+            if (seq[s[counter]][s[j]]) seq[s[counter]][s[j]]++
+            else seq[s[counter]][s[j]] = 1
+          } else {
+            for (let ltrs in seq[s[counter]]) {
+              if (seq[s[counter]][ltrs] && seq[s[counter]][ltrs] > 2) seq[s[counter]][ltrs] = 0
+            }
+            break
+          }
+        } 
+      } 
+    } 
+    counter++
   }
   
-  for (let ltr in letters) {
-    if (letters[ltr] !== 1) posSeq.push(ltr)
-  }
-  
-  for (let i = 0; i < str.length; i++) {
-    if (posSeq.includes(str[i])) res.push(str[i])
-  }
-  
-  return [posSeq, res.join('')]
+  console.log(seq) 
+  return seq
 }
 
-function twoCharaters(s) {
-
-  //remove all pairs
-  //remove all singles
-  //find all possible sequences
-    // --keep two what is the longest sequence
-    // --has to be recursive to try all possible sequences
+function twoCharacters(s) { 
+  var newS = recurxRemovePairsAndOnes(s) 
+  var dataStrtr = lettersDataStructure(newS)
   
-  var allPosLtrs = posSeqAndRemoveSingles(removePairs(s))[0]
-  var shortenString = posSeqAndRemoveSingles(removePairs(s))[1]
-  
-  return posSeqAndRemoveSingles(removePairs(s))
-  
+  return dataStrtr
 }
