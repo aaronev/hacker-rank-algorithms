1f66aa5c33b03e873ce5ca475c7723ea493868b6
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-03-13 20:05:07 -0700

    third attempt

--- algorithms/separateTheNumbers.js
+++ algorithms/separateTheNumbers.js
@@ -3,29 +3,17 @@
 function separateNumbers(s) {
   
   //Check if the first added by one equals the next number
-  //check the length of the added number and then check the next numbers in the string match the length
+  //Check the length of the added number and then check the next numbers in the string match the length
   //
   
-  var firstNum = (Number(s[0])+1).toString() 
-
-  for(let i = 1; i < firstNum.length; i++){
-    
+  var firstNum = Number(s[0]) 
+  var fNumLen = firstNum.toString().length
+  var secondNum = ''
+  
+  for(let i = 0; i < fNumLen; i++){
+    secondNum += s[i+1]
   }
   
-  var arr = []
-  
-  var i = 0
-  
-  for(let i = 0; i < s.length; i++){
-    
-  }
-  
-  while (i < arr.length) {
-    if (arr[i] < arr[i+1]) i++
-    else return separateNumbers(s.split(''))
-    return 'NO'
-  }
-  
-  
-  return `YES ${arr[0]}`
+  if (firstNum+1 != secondNum) return 'NO'    
+  return `YES ${firstNum}`
 }
