3a98392e88d698825685fe8f78adcaa826895928
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-03-25 21:00:46 -0700

    passes all test cases

--- algorithms/icecreamParlor.js
+++ algorithms/icecreamParlor.js
@@ -1,10 +1,10 @@
 //https://www.hackerrank.com/challenges/icecream-parlor/problem
 
-// passes majority test
+// passes all test cases
 function icecreamParlor(m, arr) {
   for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = i+1; j < arr.length; j++)  {
-      if (arr[i] + arr[j] === m) return [i+1, j+1].sort()
+    for (let j = i+1; j < arr.length ; j++)  {
+      if (arr[i] + arr[j] == m) return [i+1, j+1]
     }
   }
 }
