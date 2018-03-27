b6d7a4e955d6710f40c79f6da136de0f4f385712
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-03-26 18:44:06 -0700

    refactors and passes

--- algorithms/divisibleSumPairs.js
+++ algorithms/divisibleSumPairs.js
@@ -4,8 +4,8 @@
 function divisibleSumPairs(n, k, ar) {
   var valid = 0
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (i < j && (((ar[i] + ar[j]) % k) == 0)) valid++
+    for (let j = i+1; j < n; j++) {
+      if (((ar[i] + ar[j]) % k) === 0) valid++
     }
   }
   return valid
