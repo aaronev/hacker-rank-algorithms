efe1431db7a7300e414df25ff8a03139a70220f0
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-04-04 20:19:03 -0700

    adds sock merchant algo and psuedo code

---
+++ algorithms/sockMerchant.js
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/sock-merchant/problem
+
+function sockMerchant(n, ar) {
+  // Complete this function
+  //take the first value in the array
+  //check to see if this value in the array exist
+  //if it does then push it in a new array
+  //remove the values that were paired 
+  //move on to the next value of the array
+  
+}
