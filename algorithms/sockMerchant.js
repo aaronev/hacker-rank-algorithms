7be668abab5736d4efcc4c57c3311e0b1e6e2b81
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-04-05 20:32:44 -0700

    removes spaces

--- algorithms/sockMerchant.js
+++ algorithms/sockMerchant.js
@@ -1,7 +1,7 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem
 
 function sockMerchant(n, ar) {
-  // Complete this function
+  //Complete this function
   //take the first value in the array
   //check to see if this value in the array exist
   //if it does then push it in a new array
