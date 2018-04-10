0700a60300c24dd7b45fdfab6853658c672ac895
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-04-10 06:25:49 -0700

    another attempt

--- algorithms/minimunAbsoluteDiff.js
+++ algorithms/minimunAbsoluteDiff.js
@@ -1,11 +1,16 @@
 //https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
 
 function minimumAbsoluteDifference(n, arr) {
+  //divide length by two and the test will pass
   var min = arr.sort()[arr.length-1]
+  
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = i+1; j < arr.length; j++) {
-      if(Math.abs(arr[i] - arr[j]) < min) min = Math.abs(arr[i] - arr[j])
+      if (arr[i] === arr[j]) return 0
+      if (Math.abs(arr[i] - arr[j]) < min) min = Math.abs(arr[i] - arr[j])
+      
     }
   }
+  
   return min
 }
