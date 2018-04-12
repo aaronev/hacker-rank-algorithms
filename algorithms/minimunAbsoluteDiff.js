9d51dc1ba5caad2e3bb9b45be770b5aaa6e8e931
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-04-11 18:03:35 -0700

    refactors code

--- algorithms/minimunAbsoluteDiff.js
+++ algorithms/minimunAbsoluteDiff.js
@@ -3,15 +3,15 @@
 //Passes all test
 
 function minimumAbsoluteDifference(n, arr) {
+  
   var min = arr.sort()[arr.length-1]
   
   for (let i = 0; i < arr.length/2; i++) {
     for (let j = i+1; j < arr.length/2; j++) {
-      if (arr[i] === arr[j]) return 0
       if (Math.abs(arr[i] - arr[j]) < min) min = Math.abs(arr[i] - arr[j])
-      
     }
   }
-  
+ 
   return min
+  
 }
