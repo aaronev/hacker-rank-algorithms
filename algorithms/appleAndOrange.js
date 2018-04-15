8e87f911b30729db895bad0a473ad14ced31dc26
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-04-15 06:50:29 -0700

    adds psudo code

--- algorithms/appleAndOrange.js
+++ algorithms/appleAndOrange.js
@@ -1,8 +1,35 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
 
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    
+    //s and t is the length of the house
+    //apple tree left of the house
+    //orange tree is right of the house
+    //apple tree is ooint a
+    //orange is point b
+    //when a fruit falls from a tree it lands on d distance
+    //negative value is tree from left pos is tree from right
+
+
+    //givens
+    //s starting point of house
+    //t ending pint of house
+    //a location of sam's house
+    //b location of orange tree
+    //m number of apples that feel from the tree
+    //apples distance at which each apple falls from the tree
+    //n number of oranges that fell from tree
+    //oranges distance of which each oragen falls from tree
+    
+    //print the number of alls that fall on sams house the first line
+    //print the numbber of oranges that fall on sams house the second line
+
     /*
      * Write your code here.
      */
+
+     //if any of the apples/oranges land in btween s and 
+     //it lands on sams house
+     //figure out how to get the answer
 
 }
