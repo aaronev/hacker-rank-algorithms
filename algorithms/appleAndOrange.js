3d7db190957ab6b538b248b1f6f0505e87674255
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-04-17 20:01:23 -0700

    adds new approach

--- algorithms/appleAndOrange.js
+++ algorithms/appleAndOrange.js
@@ -2,36 +2,12 @@
 
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
     
-    //s and t is the length of the house
-    //apple tree left of the house
-    //orange tree is right of the house
-    //apple tree is ooint a
-    //orange is point b
-    //when a fruit falls from a tree it lands on d distance
-    //negative value is tree from left pos is tree from right
-
-
-    //givens
-    //s starting point of house
-    //t ending pint of house
-    //a location of sam's house
-    //b location of orange tree
-    //m number of apples that feel from the tree
-    //apples distance at which each apple falls from the tree
-    //n number of oranges that fell from tree
-    //oranges distance of which each oragen falls from tree
-    
-    //print the number of alls that fall on sams house the first line
-    //print the numbber of oranges that fall on sams house the second line
-
-    /*
-     * Write your code here.
-     */
-
-     //if any of the apples/oranges land in btween s and 
-     //it lands on sams house
-     //figure out how to get the answer
-
-     //still in progress
+  //a is pos apple tree
+  //b is pos of orange tree
+  //s is start of house
+  //t is end of house
+  //apples is all the apples fell
+  //oranges is all oranges that fell
+  //figure out how many fell on the house or in between s and t
 
 }
