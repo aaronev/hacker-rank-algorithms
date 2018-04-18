ef2ed8f3fb3eb0c7dc913f21452fb946010c1c9e
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-04-18 06:01:25 -0700

    added psudo code for new approach

--- algorithms/appleAndOrange.js
+++ algorithms/appleAndOrange.js
@@ -1,7 +1,7 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
 
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
-    
+
   //a is pos apple tree
   //b is pos of orange tree
   //s is start of house
@@ -9,5 +9,11 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
   //apples is all the apples fell
   //oranges is all oranges that fell
   //figure out how many fell on the house or in between s and t
-
+  
+  //variables
+  
+  //for loop
+  //if a + apples[i] > s and < t aOnHouse ++ 
+  //if b + oranges[i] >s and < t bOnHouse ++
+  
 }
