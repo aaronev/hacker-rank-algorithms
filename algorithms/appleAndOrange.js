14dab3324b62f214498078780f98355e2e6fc1ae
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-04-21 17:21:29 -0700

    add another line for commit ticks

--- algorithms/appleAndOrange.js
+++ algorithms/appleAndOrange.js
@@ -1,27 +1,21 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
 
+// Passes
+
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
-
-  //a is pos apple tree
-  //b is pos of orange tree
-  //s is start of house
-  //t is end of house
-  //apples is all the apples fell
-  //oranges is all oranges that fell
-  //figure out how many fell on the house or in between s and t
+  var apps = 0, ors = 0
   
-  //variables
-  
-  //for loop
-  //if a + apples[i] > s and < t aOnHouse ++ 
-  //if b + oranges[i] >s and < t bOnHouse ++
-
   for (let i = 0; i < apples.length; i++) {
-
+    var pos = a + apples[i]
+    if (pos >= s && pos <= t) apps++
+  }
+  //just for commit ticks
+  for (let i = 0; i < oranges.length; i++) {
+    var pos = b + oranges[i]
+    if (pos >= s && pos <= t) ors++
   }
   
-  for (let i = 0; i < oranges.length; i++) {
-
-  }
-
+  console.log(`${apps}\n${ors}`)
+  
 }
+
